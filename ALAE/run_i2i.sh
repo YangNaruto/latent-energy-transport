6b6956dbaae731d36fd6044ed5c5345370036151
Yang Zhao <yaung.zhao@gmail.com>
2021-03-03 23:05:18 -0500

    correct

--- ALAE/run_i2i.sh
+++ ALAE/run_i2i.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 export PYTHONPATH=$PYTHONPATH:$(pwd)
 export CUDA_VISIBLE_DEVICES=3
-python translation/adapt_multi.py -c celeba-hq1024 \
+python translation/adapt.py -c celeba-hq1024 \
 	OUTPUT_DIR training_artifacts/ffhq \
 	DATA.SOURCE male DATA.TARGET female \
 	LANGEVIN.STEP 15 LANGEVIN.LR 1.0 \
